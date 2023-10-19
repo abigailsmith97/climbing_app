4c53b10f42093c95ce6c8323e44011c4a8b826be
SoliriusAdmin129 <soliriusadmin129@SoliriusAdmin129s-MacBook-Pro.local>
2023-10-19 14:38:06 +0100

    clean slate

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,7 @@
 
 
 Rails.application.routes.draw do 
-  get 'pages/home'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   
-  get "up" => "rails/health#show", as: :rails_health_check
-  get 'users/new'
-  get 'users/create'
-
-  resources :users, only: [:create, :new, :show]
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
-
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
-  root 'pages#home' 
 
 
 end
